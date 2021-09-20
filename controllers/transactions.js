57af849fb8acc8f16c506dad2a5c53ffe878f5ac
George Papagapitos <george.g.papagapitos@gmail.com>
2021-09-20 16:34:22 -0500

    get transactions from db

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction');
+
 // @desc  Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    res.send(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
 }
 
 // @desc  Add transaction
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTranscation = (req, res, next) => {
-  res.send('POST transaction');
+exports.addTranscation = async (req, res, next) => {
+  
 }
 
 // @desc  Delete transaction
 // @route GET /api/v1/transactions/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE transaction');
 }
